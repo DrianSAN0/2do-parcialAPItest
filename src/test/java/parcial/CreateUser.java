972bdc263f0a78d1af2a81512afb8f76a0de0065
DrianSAN <130665784+DrianSAN0@users.noreply.github.com>
2024-10-23 18:35:36 -0400

    ejercicio 1 y 2 (Para más orden)

--- src/test/java/parcial/CreateUser.java
+++ src/test/java/parcial/CreateUser.java
@@ -1,4 +1,98 @@
 package parcial;
 
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CreateUser {
+    @Test
+    @DisplayName("Create user is successful")
+    public void createUserSuccessfully() {
+        JSONObject body = new JSONObject();
+        body.put("Email", "adrian.parcial@gmail.com");
+        body.put("FullName", "AdrianSAN");
+        body.put("Password", "0311adrian");
+
+        Response response = given()
+                .body(body.toString())
+                .post("https://todo.ly/api/user.json");
+
+        response.then()
+                .statusCode(200)
+                .body("Email", equalTo(body.get("Email")))
+                .log().all();
+    }
+
+    
+    @Test
+    @DisplayName("Create a project with token")
+    public void createAProjectWithToken() {
+        Response tokenResponse = given()
+                .auth()
+                .preemptive()
+                .basic("adrian.parcial@gmail.com", "0311adrian")
+                .log().all()
+                .when()
+                .get("https://todo.ly/api/authentication/token.json");
+
+        
+
+        tokenResponse.then()
+                .statusCode(200)
+                .log().all();
+
+        String token = tokenResponse.then().extract().path("TokenString");
+        String expirationTime = tokenResponse.then().extract().path("ExpirationTime");
+
+        JSONObject bodyProject = new JSONObject();
+        bodyProject.put("Content", "Proyecto de Prueba");
+
+        // Create a project
+        Response responseProject = given()
+                .header("Token", token)
+                .body(bodyProject.toString())
+                .log().all()
+                .when()
+                .post("https://todo.ly/api/projects.json");
+
+        responseProject.then()
+                .statusCode(200)
+                .body("Content", equalTo(bodyProject.get("Content")))
+                .log().all();
+
+        // Delete token
+        JSONObject bodyToken = new JSONObject();
+        bodyToken.put("TokenString", token);
+        bodyToken.put("UserEmail", "adrian.parcial@gmail.com");
+        bodyToken.put("ExpirationTime", expirationTime);
+
+        Response responseDeleteToken = given()
+                .header("Token", token)
+                .body(bodyToken.toString())
+                .log().all()
+                .when()
+                .delete("https://todo.ly/api/authentication/token.json");
+
+        responseDeleteToken.then()
+                .statusCode(200)
+                .body("TokenString", equalTo(bodyToken.get("TokenString")));
+
+        // Create project with deleted token
+        Response responseProject2 = given()
+                .header("Token", token)
+                .body(bodyProject.toString())
+                .log().all()
+                .when()
+                .post("https://todo.ly/api/projects.json");
+
+        responseProject2.then()
+                .statusCode(200)
+                .body("Content", equalTo(bodyProject.get("Content")))
+                .log().all();
+    }
 }
+
